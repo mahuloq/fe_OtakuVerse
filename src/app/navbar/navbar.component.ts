da937edd0e30670455898af9733605ca1d55ca59
mahuloq <95161346+mahuloq@users.noreply.github.com>
2024-03-11 20:26:23 -0500

    Starting dropdown menus on main navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../auth/services/authentication.service';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   constructor(public authService: AuthenticationService) {}
+
+  isDropdownOpen = false;
+
+  toggleDropdown(open: boolean): void {
+    this.isDropdownOpen = open;
+  }
 
   logout() {
     this.authService.logout();
