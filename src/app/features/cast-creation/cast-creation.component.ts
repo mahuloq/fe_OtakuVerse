390f78851524aa73ca88d76d2422e239b6695f41
Matt Tucker <arcanemahuloq@gmail.com>
2024-04-06 19:54:55 -0500

    Created person search service, and started working on search feature for person name input

--- src/app/features/cast-creation/cast-creation.component.ts
+++ src/app/features/cast-creation/cast-creation.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CastService } from '../../core/services/cast.service';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { PersonSearchService } from '../../core/services/person-search.service';
 
 @Component({
   selector: 'app-cast-creation',
@@ -15,6 +17,7 @@ export class CastCreationComponent implements OnInit {
 
   castForm = new FormGroup({
     person_id: new FormControl(null),
+    person_name: new FormControl(null),
     anime_id: new FormControl(0),
     role: new FormControl(null),
     character: new FormControl(null),
@@ -23,7 +26,8 @@ export class CastCreationComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private castService: CastService
+    private castService: CastService,
+    private personSearch: PersonSearchService
   ) {}
 
   ngOnInit(): void {
@@ -32,7 +36,28 @@ export class CastCreationComponent implements OnInit {
     this.castForm.patchValue({
       anime_id: this.animeId,
     });
+
+    this.castForm
+      .get('person_name')
+      ?.valueChanges.pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((personName) => this.personSearch.searchByName(personName!))
+      )
+      .subscribe(
+        (response) => {
+          // Handle search results here
+          console.log('Search results:', response);
+          // Update UI to display search results or suggestions
+        },
+        (error) => {
+          console.error('Error:', error);
+          // Handle error if any
+        }
+      );
   }
 
-  onCreateCrew() {}
+  onCreateCrew() {
+    // Implement creation logic here, including using person_id from the form
+  }
 }
